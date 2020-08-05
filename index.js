e2312aabcb81bfed387da37aa26399dfa5771ed0
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-05 07:32:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function walkToPark(dogName, dogBreed) {
 
 function throwFrisbee(dogName, dogBreed) {
     console.log(`throw frisbee to ${dogName}`);
-    return `Throw the frisbee to ${dogName} the ${dogBreed}`
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
 }
 
 function walkHome(dogName, dogBreed) {
