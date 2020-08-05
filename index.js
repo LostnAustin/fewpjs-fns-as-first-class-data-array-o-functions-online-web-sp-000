37b09a998ff357058d1faaac21d85d9f08bb1c20
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-05 07:07:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-// function leashDog(dogName, dogBreed) {
-//   console.log(`put leash on ${dogName}`);
-// }
+function leashDog(dogName, dogBreed) {
+  console.log(`put leash on ${dogName}`);
+}
+
+function walkToPark(dogName, dogBreed) {
+  ;
+}
+
+function throwFrisbee(dogName, dogBreed) {
+  ;
+}
+
+function walkHome(dogName, dogBreed) {
+  ;
+}
+
+function unleashDog(dogName, dogBreed) {
+  ;
+}
