73125b5007b71de36f1e67187dd1c0f7473559f2
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-05 07:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 
-function leashDog(dogName, dogBreed) {
-  console.log(`put leash on ${dogName}`);
-}
+// function leashDog(dogName, dogBreed) {
+//   console.log(`put leash on ${dogName}`);
+// }
